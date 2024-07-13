c05c3e7bd54258bffa52c34450c7fb007868b219
Yousuf Mohiuddin <mohiuddy@uoguelph.ca>
2024-07-13 00:16:57 -0400

    First commit

---
+++ linked_lists/singly_linked_list.c
@@ -0,0 +1,71 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct Node {
+    int value;
+    struct Node *next;
+} SLList;
+
+SLList *create_node(int value);
+void insert_node(SLList *preNode, SLList *insert_node);
+void delete_node(SLList *preNode, SLList *delete_node);
+void append(SLList *append, SLList *insert_node);
+void free_list(SLList *head);
+void print_list(SLList *printNode);
+
+
+
+SLList *create_node(int value) {
+    SLList *new_node;
+    new_node = malloc(sizeof(SLList)); //allocating space for node
+    if(new_node == NULL) 
+    {
+        return NULL;
+    }
+    new_node->value = value;
+    new_node->next = NULL;
+    return new_node;
+}
+
+void insert_node(SLList * node_before_insert, SLList * node_to_insert) {
+    node_to_insert->next = node_before_insert->next;
+    node_before_insert->next = node_to_insert;
+}
+
+void delete_node(SLList * node_before_delete, SLList *node_to_delete) {
+    node_before_delete->next = node_to_delete->next;
+}
+
+
+
+
+void append(SLList *head, SLList *node_to_append) {
+    SLList *current_node = head;
+    while (current_node->next != NULL) 
+    {
+        current_node = current_node->next;
+    }
+    current_node->next = node_to_append;
+    current_node->next->value = node_to_append->value;
+    node_to_append->next = NULL;
+}
+
+void free_list(SLList * head) {
+    SLList *tmp;
+    while (head)
+    {
+        tmp = head;
+        head = head->next;
+        free(tmp);
+    }
+}
+
+void print_list(SLList * head) {
+    SLList *tmp = head;
+    while (tmp != NULL) 
+    {
+        printf("%d - ",tmp->value);
+        tmp = tmp->next;
+    }
+    printf("\n");
+}
---
+++ linked_lists/stack.c
@@ -0,0 +1,70 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct Node {
+    int value;
+    struct Node *next;
+} Stack;
+
+Stack *create_node(int value);
+void push(Stack **head, int value);
+void print_list(Stack *print_node);
+void free_list(Stack *head);
+void pop(Stack **head);
+
+Stack *create_node(int value) 
+{
+    Stack *new_node;
+    new_node = malloc(sizeof(Stack)); //aStackocating space for node
+    if(new_node == NULL) 
+    {
+        return NULL;
+    }
+    new_node->value = value;
+    new_node->next = NULL;
+    return new_node;
+}
+
+// Single Pointer - modifies head
+// double pointer - modifies where head points to
+void push(Stack **head, int value) 
+{
+    Stack *new_node = malloc(sizeof(Stack));
+    new_node->value = value;
+    new_node->next = *head;
+    *head = new_node;
+}
+
+void pop (Stack **head) 
+{
+    Stack *pop_node;
+    pop_node = (*head);
+    (*head) = (*head)->next;
+    free(pop_node);
+}
+
+
+void print_list(Stack *print_node) 
+{
+    Stack *temp_node = print_node;
+    while (temp_node != NULL) 
+    {
+        printf("%d - ",temp_node->value);
+        temp_node = temp_node->next;
+    }
+    printf("\n");
+}
+
+
+void free_list(Stack *head) 
+{
+    Stack *temp_node;
+    while (head)
+    {
+        temp_node = head;
+        head = head->next;
+        free(temp_node);
+    }
+}
+
+
